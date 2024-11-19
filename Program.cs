cd1d5cf65c9f13495837d637112a8fbb0e340580
Redline1e <ahorobiichenko@gmail.com>
2024-11-19 18:39:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿class Rectangle
+{
+    public virtual int Width { get; set; }
+    public virtual int Height { get; set; }
+
+    public int GetRectangleArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Square
+{
+    public int Side { get; set; }
+
+    public int GetArea()
+    {
+        return Side * Side;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle rect = new Rectangle() { Width = 5, Height = 10 };
+        Console.WriteLine(rect.GetRectangleArea()); // 50
+
+        Square square = new Square() { Side = 5 };
+        Console.WriteLine(square.GetArea()); // 25
+
+        Console.ReadKey();
+    }
+}
